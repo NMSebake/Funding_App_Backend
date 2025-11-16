188d780a0ad754c73f801d1be959210c81b32701
NMSebake <sebaketsime@gmail.com>
2025-11-16 21:36:09 +0200

    Made changes to types/express/index.d.ts

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -4,7 +4,7 @@ declare global {
   namespace Express {
     interface Request {
       file?: Express.Multer.File;
-      files?: Express.Multer.File[];
+      files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
     }
   }
 }
